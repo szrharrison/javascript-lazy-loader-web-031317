bdebcd8af4138a91de8333a24f4633bd80103b2b
szrharrison <s.z.r.harrison@gmail.com>
2017-05-01 16:32:59 -0400

    Added my answer but won't pass local tests
    However, does pass learn -b tests

--- public/js/cars.js
+++ public/js/cars.js
@@ -1,22 +1,35 @@
 "use strict";
 
 // this is the base API url
-var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
+var baseUrl = "https://mimeocarlisting.azurewebsites.net/api/cars/";
 
 function formatCars(carsJSON) {
   // this function shold return a string of properly formatted html
   // refer to app/views/index.erb lines 16 - 22 for an example of how
-  // to format three cars, each in a div with a class "col-md-4", in a 
+  // to format three cars, each in a div with a class "col-md-4", in a
   // div with a class "row"
+  let cars = carsJSON.map(function(car, i, carsJSON) {
+    return `<div class="col-md-4 car"><h2>${car.Make}</h2><p><strong>Model:</strong> ${car.Model}</p><p><strong>Year:</strong> ${car.Year}</p></div>`
+  })
+  return `<div class="row">${cars.join('')}</div>`
 }
 
 function addCarsToDOM(carsJSON) {
-  // this function should pass carsJSON to formatCars() and then 
+  // this function should pass carsJSON to formatCars() and then
   // add the resulting HTML to the div with an id of "cars"
+  $('#cars').append( formatCars( carsJSON ) )
 }
-
+let i = 2
 function fetchJSON() {
   // this function will make the ajax call
   // on success of the ajax call, it will pass the returned data
   // to addCarsToDOM()
+  i += 1
+  let url = `${baseUrl}${i}/3`
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: addCarsToDOM
+  })
+
 }
--- public/js/on-click.js
+++ public/js/on-click.js
@@ -3,4 +3,5 @@
 $(document).ready(function() {
   // add click listener here
   // it should call on fetchJSON()
+  $('button#load-cars').on( 'click', fetchJSON )
 });
